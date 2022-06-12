a1dbf337b553ca311e6b1c9dd01a01cc7df34793
IrfEazy <irfan.cela.5@gmail.com>
2022-06-12 18:16:59 +0200

    Server classes.

---
+++ server/src/Protocol.java
@@ -0,0 +1,110 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class Protocol implements Closeable, Runnable {
+    private static final ArrayList<Protocol> protocolList = new ArrayList<>();
+
+    private final GregorianCalendar time = new GregorianCalendar();
+
+    private BufferedReader in;
+    private File log;
+    private PrintWriter out;
+    private Socket serverToClient;
+    private String userName;
+
+    public Protocol() {
+        try {
+            log = new File("log.txt");
+            if (!log.exists()) {
+                log.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Protocol(@NotNull Socket serverToClient) {
+        try {
+            this.in = new BufferedReader(new InputStreamReader(serverToClient.getInputStream()));
+            this.out = new PrintWriter(serverToClient.getOutputStream(), true);
+            selectUsername();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.serverToClient = serverToClient;
+        protocolList.add(this);
+    }
+
+    private void selectUsername() {
+        String name = "";
+
+        while (name.equals("")) {
+            this.out.println("Insert username: ");
+            try {
+                name = this.in.readLine();
+                for (Protocol protocol : protocolList) {
+                    if (protocol.getUserName().equals(name)) {
+                        this.out.println("Username already taken.");
+                        name = "";
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.userName = name;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.serverToClient.close();
+        protocolList.remove(this);
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.out.println("Insert strings. Insert 'quit' to exit.");
+            String request;
+
+            while ((request = this.in.readLine()) != null) {
+                this.clientMessage(request);
+                saveRequest(request);
+                String reply = request.toUpperCase();
+                this.out.println(reply);
+                if (reply.equals("QUIT")) break;
+            }
+
+            this.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveRequest(String request) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(log, true));
+            writer.println("<" + time.getTime() + " - " + this.serverToClient.getInetAddress() + ">:<" + request);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clientMessage(String s) {
+        this.out.println(this.serverToClient.getInetAddress() + ":" + this.serverToClient.getPort() + "\t" + this.userName + ":\t" + s);
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+}
---
+++ server/src/Server.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public static void main(String[] args) {
+        startServer();
+    }
+
+    /**
+     * Start the server creating a socket on the port 1234. One instanced it waits for new
+     * clients to connect to the server and sent the request of them to a new thread created automatically by
+     * the system. It next starts a conversation with the client since he wants to left the server.
+     */
+    private static void startServer() {
+        try (ServerSocket server = new ServerSocket(1234); Protocol protocol = new Protocol()) {
+            System.out.println("Waiting for clients...");
+            ExecutorService service = Executors.newCachedThreadPool();
+
+            do {
+                Socket serverToClient = server.accept();
+                System.out.println("New client connected to the server: " + serverToClient.getInetAddress() + ":" + serverToClient.getPort());
+                Protocol clientProtocol = new Protocol(serverToClient);
+                service.submit(clientProtocol);
+            } while (true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
